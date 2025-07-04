d7ff725cabe5df0e8b3c221ab2171cb21f4ca28f
MD ABDULLAH ALL MAMUN <mamun700263@gmail.com>
2025-07-05 04:28:38 +0600

    chore #9: app: changed names and location of some file

--- app/models/amazon/amazon_searching_models.py
+++ app/models/amazon/amazon_searching_models.py
@@ -2,8 +2,10 @@ from typing import List
 from pydantic import BaseModel, Field
 
 class AmazonSearchResult(BaseModel):
-    name:str
+    id: int
+    name:str = Field(..., min_length=3)
     url:url 
     image_url : url
+    description: str = Field(..., min_length=1)
     rating:float
     rated_by : int

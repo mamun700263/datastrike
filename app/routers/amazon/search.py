188e9b1e07528f8437895c0ac547ff4ef15f9e2d
MD ABDULLAH ALL MAMUN <mamun700263@gmail.com>
2025-07-05 15:53:16 +0600

    refactor #10: app: updated the routers and saving methods

--- app/routers/amazon/search.py
+++ app/routers/amazon/search.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter
 
 from app.core.orchestrators import AmazonSearchOrchestrator
+from app.core.data_exporters import InMemoryExporter
 
 router = APIRouter(prefix="/amazon", tags=["Amazon Search"])
 
 @router.post("/search")
-def search_amazon(query: str , page: int,):
+def search_amazon(query: str , page: int, file_name: str =None):
     orchestrator = AmazonSearchOrchestrator(query ,page)
     result = orchestrator.run()
+    # if file_name is not None:
+    InMemoryExporter.export(result,'csv')
     return result
